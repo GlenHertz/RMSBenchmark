621fe8b9ab37de3957579541cfd8ee4be7a1e193
Glen Hertz <glen.hertz@juliacomputing.com>
2022-11-23 11:14:25 -0500

    Fix constant fold with gcc

--- rms/rms.c
+++ rms/rms.c
@@ -26,8 +26,6 @@ int main() {
     double tstop, dt, freq, t;
     size_t element_size = sizeof tstop;
     double val;
-    double *xs_gen;
-    double *ys_gen;
     double *xs;
     double *ys;
     double *results;
@@ -39,53 +37,30 @@ int main() {
     freq = 1000;
     tstop = 1/ (double) freq;
     dt = tstop / (double) (N-1);
-    xs_gen = malloc(N*sizeof(double));
-    ys_gen = malloc(N*sizeof(double));
-    xs = malloc(N*sizeof(double));
-    ys = malloc(N*sizeof(double));
+    xs = malloc(N*element_size);
+    ys = malloc(N*element_size);
 
     for (i = 0; i < N; i++) {
         t = dt * i;
-        xs_gen[i] = t;
-        ys_gen[i] = sin(2*M_PI*freq*t);
+        xs[i] = t;
+        ys[i] = sin(2*M_PI*freq*t);
     }
 
-    /* write to file to prevent constant folding */
-    char file_name[] = "xs_ys.bin";
-    FILE * fp;
-
-    fp = fopen(file_name, "wb");
-    if (fp == NULL) { /* If an error occurs during the file creation */
-        fprintf(stderr, "fopen() failed for '%s'\n", file_name);
-        return 1;
-    } else {
-        fwrite(xs_gen, element_size, N, fp);
-        fwrite(ys_gen, element_size, N, fp);
-        fclose(fp);
-    }
-
-    fp = fopen(file_name, "r");
-    if (fp == NULL) { /* If an error occurs during the file creation */
-        fprintf(stderr, "fopen() failed for '%s'\n", file_name);
-        return 1;
-    } else {
-        fread(xs, element_size, N, fp);
-        fread(ys, element_size, N, fp);
-        fclose(fp);
-    }
 
     /* run benchmark */
     trials = 500;
-    results = malloc(trials*sizeof(double));
+    results = malloc(trials*element_size);
     val = rms(xs, ys, N);
     t1 = clock();
     for (i=0; i < trials; i++)
         results[i] = rms(xs, ys, N);
     t2 = clock();
-    char file_name2[] = "results.bin";
-    fp = fopen(file_name2, "wb");
+
+    /* print out result to prevent constant folding */
+    char fname[] = "results.bin";
+    FILE * fp = fopen(fname, "wb");
     if (fp == NULL) { /* If an error occurs during the file creation */
-        fprintf(stderr, "fopen() failed for '%s'\n", file_name2);
+        fprintf(stderr, "fopen() failed for '%s'\n", fname);
         return 1;
     } else {
         fwrite(results, element_size, trials, fp);
@@ -93,6 +68,6 @@ int main() {
     }
 
     ns = (double) (t2 - t1) / (double) CLOCKS_PER_SEC / (double) trials / (double) N * 1e9;
-    printf("gcc %s             for rms=%.16f : %8.7f ns per iteration average over %ld trials of %ld points\n", VERSION, val, ns, trials, N);
+    printf("gcc %s             for rms=%.16f : %5.3f ns per iteration average over %ld trials of %ld points\n", VERSION, val, ns, trials, N);
     return 0;
 }
