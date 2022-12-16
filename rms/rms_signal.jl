f14bb33264a431fa331de146aa1d03e35db201fa
Glen Hertz <glen.hertz@juliacomputing.com>
2022-12-16 15:12:19 -0500

    Update

--- rms/rms_signal.jl
+++ rms/rms_signal.jl
@@ -10,15 +10,15 @@ struct Signal{X,Y}
 end
 length(s::Signal) = length(s.x)
 getindex(s::Signal, i::Int) = (x=s.x[i], y=s.y[i])
-
+using LoopVectorization
 function rms(s::Signal)
-    area = zero(s.x[1] * s.y[1]^2 / 3)
+    area = zero(first(s.x) * first(s.y)^2 / 3)
     for i in 2:length(s)
-        p1 = s[i-1]
-        p2 = s[i]
-        dx = p2.x - p1.x
-        dy = p2.y - p1.y
-        area += dx*(dy^2/3 + p1.y*p2.y)
+        x₁, y₁ = s[i-1]
+        x₂, y₂ = s[i]
+        Δx = x₂ - x₁
+        Δy = y₂ - y₁
+        area += Δx*(Δy^2/3 + y₁*y₂)
     end
     sqrt(area/(last(xs) - first(xs)))
 end
@@ -26,29 +26,11 @@ end
 
 
 # Create sine wave:
-xs = 0:10^-6:1
+xs = collect(0:10^-6:1)
 ys = sin.(2pi*1000*xs)
 sig = Signal(xs, ys)
 val = rms(sig)
 
-#using Unitful, Unitiful.DefaultSymbols
-#xs = xs .* 1s
-#ys = ys .* 1V
 
-
-# Run benchmark:
-val = rms(sig)  # first time to compile
-trials = 300
-t = @elapsed for i in 1:trials
-    rms(sig)
-end
-ns = t / trials / N * 1e9
-
-println("Julia  $VERSION rms = $val : $(lpad(round(ns, sigdigits=2), 5)) ns per iteration average over $trials trials of $N points")
-
-#using BenchmarkTools
-#res = @benchmark rms(xs, ys)
-#t = mean(res)
-## Plot histogram:
-#using UnicodePlots
-#histogram(res.times)
+xs = collect(xs)
+include("benchmark.jl")
