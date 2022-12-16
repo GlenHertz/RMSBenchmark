c10d430a2cab82fd8ac38c2ad5abe00d8601feb2
Glen Hertz <glen.hertz@juliacomputing.com>
2022-12-16 14:58:43 -0500

    Update

--- rms/rms_array.jl
+++ rms/rms_array.jl
@@ -1,10 +1,11 @@
 function rms(xs, ys)
     @assert length(xs) == length(ys) "dimension mismatch"
-    x1 = @view xs[begin:end-1]
-    y1 = @view ys[begin:end-1]
-    x2 = @view xs[begin+1:end]
-    y2 = @view ys[begin+1:end]
-    area = sum(@. (x2 - x1) * ((y2 - y1)^2/3 + y1*y2))
+    x₁ = @view xs[begin:end-1]
+    y₁ = @view ys[begin:end-1]
+    x₂ = @view xs[begin+1:end]
+    y₂ = @view ys[begin+1:end]
+    areas = @. (x₂ - x₁) * ((y₂ - y₁)^2/3 + y₁*y₂)
+    area = sum(areas)
     sqrt(area/(xs[end]- xs[begin]))
 end
 
