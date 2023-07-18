1d957638a3c087beb2ce37af3693933fda2f7210
Glen Hertz <glen.hertz@juliahub.com>
2023-07-18 14:28:46 +0000

    Add Julia ThreadsX version

---
+++ rms/rms_threadsx.jl
@@ -0,0 +1,14 @@
+using ThreadsX
+function rms(xs, ys)
+    @assert length(xs) == length(ys) "dimension mismatch"
+    sqrt(ThreadsX.sum((xs[i] - xs[i-1])*((ys[i]-ys[i-1])^2/3 + ys[i-1]*ys[i]) for i in 2:length(xs))/(xs[end] - xs[1]))
+end
+
+# generate a 1kHz sine wave from 0 to 1 second (with 1us steps):
+xs = 0:10^-6:1
+ys = sin.(2pi * 1000 * xs)
+val = rms(xs, ys)
+
+
+xs = collect(xs)
+include("benchmark.jl")
