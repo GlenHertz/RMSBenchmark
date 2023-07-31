a22e9e08aaffd2146dd0438e01e9006569044f2a
Glen Hertz <glen.hertz@juliahub.com>
2023-07-31 15:18:44 -0400

    Don't need `@inbounds` for `@fastmath`

--- rms/rms_fastmath.jl
+++ rms/rms_fastmath.jl
@@ -1,7 +1,7 @@
 function rms(xs, ys)
     @assert length(xs) == length(ys) "dimension mismatch"
     area = zero(xs[1] * ys[1]^2 / 3)
-    @inbounds @fastmath for i in 2:length(xs)
+    @fastmath for i in 2:length(xs)
         Δx = xs[i] - xs[i-1]
         y1 = ys[i-1]
         y2 = ys[i]
